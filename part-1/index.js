1d84cb5450641d31ea623bb0ae0838ccbb257ca3
alexm <alex-m@outlook.com>
2023-07-10 16:44:49 -0700

    Use implicit return for FirstComponent function; created NamedComponent and App components; render App component on root

--- part-1/index.js
+++ part-1/index.js
@@ -6,11 +6,27 @@
  */
 
 
-const FirstComponent = () => {
-    return <h1>My very first component</h1>;
+const FirstComponent = () => (
+    <h1>My very first component</h1>
+);
+
+
+const NamedComponent = (props) => {
+    const { name } = props;
+    return <p>My name is {name}</p>
+};
+
+
+const App = () => {
+    return (
+        <div>
+            <FirstComponent />
+            <NamedComponent name="Bob"/>
+        </div>
+    );
 }
 
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<FirstComponent />);
+root.render(<App />);
