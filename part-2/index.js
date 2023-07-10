581093d30d76e21ed8d74c25d6fa3f7443a462c8
alexm <alex-m@outlook.com>
2023-07-10 16:58:10 -0700

    Create file with component definitions and render some components

---
+++ part-2/index.js
@@ -0,0 +1,39 @@
+// Ioana A Mititean
+// 37.2 - React and JSX Intro
+
+/**
+ * JSX components for Part 2.
+ */
+
+
+const Tweet = (props) => {
+    const {username, name, date, message} = props;
+    return (
+        <div>
+            <h2>Tweet Info</h2>
+            <ul>
+                <li>Created by (username): {username}</li>
+                <li>Created by (name): {name}</li>
+                <li>Date created: {date}</li>
+                <li>Message body: {message}</li>
+            </ul>
+        </div>
+    )
+}
+
+
+const App = () => {
+    return (
+        <div>
+            <Tweet username="A000" name="Alice" date="7-10-23" message="Hello world" />
+            <Tweet username="B111" name="Bob" date="6-19-23" message="Happy Birthday to me!" />
+            <Tweet username="C222" name="Charlie" date="5-5-23" message="Cinco de Mayo" />
+            <Tweet username="D333" name="Daisy" date="4-01-23" message="April Fool's!" />
+        </div>
+    );
+}
+
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
