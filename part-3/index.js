b22ecabda4812c026c17cab7bb2fa06311d4c066
alexm <alex-m@outlook.com>
2023-07-10 18:09:00 -0700

    Add a hobbies prop for Person component; display hobbies for each Person in a list; add key property to each list item

--- part-3/index.js
+++ part-3/index.js
@@ -8,13 +8,17 @@
 
 const Person = (props) => {
 
-    const {name, age} = props;
+    const {name, age, hobbies} = props;
 
     return (
         <div>
             <p>Learn some information about this person.</p>
             <h3>{name.slice(0, 8)}, age {age}</h3>
-            <h3>{age >= 18 ? "Please go vote!" : "You must be at least 18."}</h3>
+            <h3>Hobbies:</h3>
+            <ul>
+                {hobbies.map(hobby => <li key={hobby.id}>{hobby.text}</li>)}
+            </ul>
+            <h3>{age >= 18 ? "Please go vote!" : "You must be at least 18 to vote."}</h3>
         </div>
     )
 }
@@ -23,9 +27,27 @@ const Person = (props) => {
 const App = () => {
     return (
         <div>
-            <Person name="Alice" age={22}/>
-            <Person name="Bob" age={17} />
-            <Person name="ClaireDaBossIsHerName" age={18} />
+            <Person
+                name="Alice"
+                age={22}
+                hobbies={[{id: 1, text: "hiking"}, {id: 2, text: "video games"}]} />
+
+            <Person
+                name="Bob"
+                age={17}
+                hobbies={[{id: 1, text: "golf"}]} />
+
+            <Person
+                name="ClaireDaBossIsHerName"
+                age={18}
+                hobbies={
+                    [
+                        {id: 1, text: "basketball"},
+                        {id: 2, text: "volleyball"},
+                        {id: 3, text: "swimming"},
+                        {id: 4, text: "drawing"}
+                    ]
+                } />
         </div>
     )
 };
