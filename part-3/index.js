5e7017ca0ae8adc3e1c2cb42e3b1b4accff487e1
alexm <alex-m@outlook.com>
2023-07-10 17:56:16 -0700

    Create Person component, which displays name and age info; render 3 Person components in App component

--- part-3/index.js
+++ part-3/index.js
@@ -6,13 +6,26 @@
  */
 
 
+const Person = (props) => {
 
+    const {name, age} = props;
+
+    return (
+        <div>
+            <p>Learn some information about this person.</p>
+            <h3>{name.slice(0, 8)}, age {age}</h3>
+            <h3>{age >= 18 ? "Please go vote!" : "You must be at least 18."}</h3>
+        </div>
+    )
+}
 
 
 const App = () => {
     return (
         <div>
-
+            <Person name="Alice" age={22}/>
+            <Person name="Bob" age={17} />
+            <Person name="ClaireDaBossIsHerName" age={18} />
         </div>
     )
 };
