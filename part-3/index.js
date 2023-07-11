30a56e47a297e43aef42dc39fa1540f437a25eaf
alexm <alex-m@outlook.com>
2023-07-10 17:38:57 -0700

    Create file with App component definition; render App component

---
+++ part-3/index.js
@@ -0,0 +1,23 @@
+// Ioana A Mititean
+// 37.2 - React and JSX Intro
+
+/**
+ * JSX components for Part 3.
+ */
+
+
+
+
+
+const App = () => {
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
